c286e0fe1b205f64f13b7e1b8eb38aa7d628055e
Manoj Nambiar <manojnambiar@Manojs-MBP.attlocal.net>
2019-04-13 22:05:10 -0400

    added insert and delete

--- linkedlist.py
+++ linkedlist.py
@@ -38,18 +38,60 @@ class linkedlist(object):
             return index
         else:
             return -1
+    def insert(self,element,position=0):
+        """   
+        first element is considered to be at postion 0
+
+        """
+        counter = 0
+        if position == 0:
+            headElement = self.head
+            element.next = headElement
+            self.head = element
+        else:
+            current = self.head
+            while current.next:
+                if counter == (position - 1):
+                    element.next = current.next
+                    current.next = element
+                current = current.next
+                counter += 1
+
+    def delete(self,value):
+        """
+        deletes only the fist match
+
+        """
+        current = self.head
+        if current.value == value: # if the element is the header
+            self.head = current.next
+        else:
+            while current.next:
+                if current.next.value == value:
+                    current.next = current.next.next
+                    break
+                current = current.next
 
 def main():
 
     n = node(10)
     k = node(20)
     j = node(30)
+    m = node(40)
+    a = node(50)
 
     l1 = linkedlist(n)
     l1.append(k)
     l1.append(j)
+    #l1.display()
+    #print(l1.search(40))
+    l1.insert(m)
+    #l1.display()
+    l1.insert(a,position=2)
+    #l1.display()
+    l1.delete(10)
+    l1.delete(40)
     l1.display()
-    print(l1.search(40))
 
 if __name__ == "__main__":
     main()
