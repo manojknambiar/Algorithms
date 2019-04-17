4ceef771eaf632e607f7b7c5515fd41c6c072940
Manoj Nambiar <manojkopparambil@gmail.com>
2019-04-16 23:47:58 -0400

    no change

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -44,7 +44,7 @@ def main():
     d1 = doublyLinkedList(a1)
     d1.insert(a2,position=0)
     d1.insert(a3,position=1)
-    #d1.insert(a4,position=1)
+    d1.insert(a4,position=1)
     d1.display()
 
 
